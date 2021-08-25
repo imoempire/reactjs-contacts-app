692300f0fe149c4c7f299040dfdaa1c9d65ba269
imoempire <mrimoempire@gmail.com>
2021-08-25 14:45:57 +0000

    import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
+import Contact from './Components/Contact'
+import ContactForm from './Components/ContactForm';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+     contacts: [
+       { 
+         Name: 'James',
+         Phone: '0244945255',
+         Location: 'Accra'
+       }
+     ]
+    }
+  };
+  handleAdd=(newContact)=>{
+    this.setState({
+      contacts: [...this.state.contacts, newContact]
+    })
+  }
+  render() {
+    const contacts = this.state.contacts.map((contact, index) => {
+      return(
+        <Contact contacts={contact} index={index}/>
+      )
+    } )
+    return (
+      <div>
+        <ContactForm addcontact={this.handleAdd}/>
+        <h1>All Contacts</h1>
+        {contacts}
+      </div>
+    );
+  }
 }
 
 export default App;
