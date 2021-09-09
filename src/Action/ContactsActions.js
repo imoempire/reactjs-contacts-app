ee4a33bcfa0d82d731c8fddecbd05367f2dd80f5
imoempire <mrimoempire@gmail.com>
2021-09-09 13:53:28 +0000

    add action file

---
+++ src/Action/ContactsActions.js
@@ -0,0 +1,25 @@
+export const ADD_CONTACT = "ADD_CONTACT";
+export const DELETE_CONTACT ="DELETE_CONTACT";
+export const EDIT_CONTACT = "EDIT_CONTACT"
+
+
+export const addContact = (contact)=>{
+    contact.id = Math.random().toString();
+    return{
+        type: ADD_CONTACT,
+        payload: contact
+    }
+};
+export const deleteContact = (contactId)=>{
+    return{
+        type: DELETE_CONTACT,
+        payload: contactId
+    }
+};
+
+export const editContact = (updateContact)=>{
+    return{
+        type: EDIT_CONTACT,
+        payload: updateContact
+    }
+};
